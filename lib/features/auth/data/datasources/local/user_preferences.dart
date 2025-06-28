bb892e7e2f9b56ee93c9241662289582ba8e05ab
ad-ng <adolphengoga@gmail.com>
2025-06-28 08:54:11 +0200

    api to log in a user

--- lib/features/auth/data/datasources/local/user_preferences.dart
+++ lib/features/auth/data/datasources/local/user_preferences.dart
@@ -1,31 +1,30 @@
+import 'dart:convert';
 import 'package:nearme_fn/features/auth/data/models/user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
 
 class UserPreferences {
-  // Save user to SharedPreferences
+  /// Save user to SharedPreferences
   Future<void> saveLocalUser(UserModel user) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String userJson = json.encode(
-      user.toJson(),
-    ); // Convert UserModel to JSON string
-    await prefs.setString('user', userJson); // Save the JSON string
+    final prefs = await SharedPreferences.getInstance();
+    final userJson = json.encode(user.toMap());
+    await prefs.setString('user', userJson);
   }
 
-  // Get user from SharedPreferences
+  /// Get user from SharedPreferences
   Future<UserModel?> getLocalUser() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? userJson = prefs.getString('user');
+    final prefs = await SharedPreferences.getInstance();
+    final userJson = prefs.getString('user');
 
     if (userJson != null) {
-      return UserModel.fromJson(userJson);
+      final userMap = json.decode(userJson) as Map<String, dynamic>;
+      return UserModel.fromMap(userMap);
     }
-    return null; // Return null if no user data found
+    return null;
   }
 
-  // Remove the user from SharedPreferences
+  /// Remove the user from SharedPreferences
   Future<void> removeLocalUser() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final prefs = await SharedPreferences.getInstance();
     await prefs.remove('user');
   }
 }
--- lib/features/auth/data/datasources/remote/auth_api_service.dart
+++ lib/features/auth/data/datasources/remote/auth_api_service.dart
@@ -1,22 +1,33 @@
 import 'package:dio/dio.dart';
 import 'package:nearme_fn/conf/dio/dioService.dart';
+import 'package:nearme_fn/features/auth/data/datasources/local/tokenstore.dart';
+import 'package:nearme_fn/features/auth/data/datasources/local/user_preferences.dart';
+import 'package:nearme_fn/features/auth/data/models/user_model.dart';
 
+/// a class that performs all api calls related to auth
 class AuthApiService {
   final Dio _dio = DioService.instance.dio;
 
-  Future login() async {
+  ///logging in a user
+  Future<UserModel> login() async {
     try {
-      final response = await _dio.post(
+      final response = await _dio.post<Map<String, dynamic>>(
         '/auth/login',
         data: {'email': 'adolphengoga@gmail.com', 'password': 'test@123'},
       );
 
-      print(response.data);
-      return response.data;
+      final token = response.data?['token'] as String;
+      final dataJson = response.data?['data'] as Map<String, dynamic>;
+      final currentUser = UserModel.fromMap(dataJson);
+
+      await TokenStore.setToken(token);
+      await UserPreferences().saveLocalUser(currentUser);
+
+      return currentUser;
     } on DioException catch (e) {
-      return e.message.toString();
+      throw Exception(e.message);
     } catch (e) {
-      return Future.error(e);
+      throw Exception('Something went wrong: $e');
     }
   }
 }
