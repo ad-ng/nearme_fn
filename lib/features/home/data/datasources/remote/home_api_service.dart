013d93bddd94c3baa8b8becd625779bb69f69554
ad-ng <adolphengoga@gmail.com>
2025-07-09 12:55:50 +0200

    fetching sub category items

--- lib/features/home/data/datasources/remote/home_api_service.dart
+++ lib/features/home/data/datasources/remote/home_api_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:nearme_fn/conf/dio/dioService.dart';
+import 'package:nearme_fn/features/home/data/models/place_item_model.dart';
 import 'package:nearme_fn/features/home/data/models/subcategory_model.dart';
 
 ///
@@ -20,6 +21,31 @@ class HomeApiService {
             .map(
               (json) => SubcategoryModel.fromMap(json as Map<String, dynamic>),
             )
+            .toList();
+      } else {
+        throw Exception('Expected a list but got ${dataJson.runtimeType}');
+      }
+    } on DioException catch (e) {
+      throw Exception('Something went wrong: $e');
+    } catch (e) {
+      throw Exception('Something went wrong: $e');
+    }
+  }
+
+  ///
+  Future<List<PlaceItemModel>> fetchSubCategoryItems(
+    String subCategoryName,
+  ) async {
+    try {
+      final response = await _dio.get<Map<String, dynamic>>(
+        '/category/subcategory/$subCategoryName',
+      );
+
+      final dataJson = response.data?['data'];
+
+      if (dataJson != null && dataJson is List) {
+        return dataJson
+            .map((json) => PlaceItemModel.fromMap(json as Map<String, dynamic>))
             .toList();
       } else {
         throw Exception('Expected a list but got ${dataJson.runtimeType}');
